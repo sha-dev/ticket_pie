79d70481d7dfeb72c44a7990c353b8a93969c425
shade <sha-dev@gmail.com>
2016-07-13 09:19:51 +0900

    [add] First commit
    SpringBoot ひな形

---
+++ src/main/java/de/sha/ticketpie/TicketPieApplication.java
@@ -0,0 +1,12 @@
+package de.sha.ticketpie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketPieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketPieApplication.class, args);
+	}
+}
