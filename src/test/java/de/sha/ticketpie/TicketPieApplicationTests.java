dd11778c98b951a398c737e09f6ddb82f40a0676
sato_hiroki <sato_hiroki@scsac.co.jp>
2016-07-13 09:19:51 +0900

    [add] First commit
    SpringBoot ひな形

---
+++ src/test/java/de/sha/ticketpie/TicketPieApplicationTests.java
@@ -0,0 +1,16 @@
+package de.sha.ticketpie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TicketPieApplication.class)
+public class TicketPieApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
