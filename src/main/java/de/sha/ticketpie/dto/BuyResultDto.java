7bd91e367753692901b925396e03a19d3694d6bf
shade <sha-dev@gmail.com>
2016-07-17 20:58:17 +0900

    [add] BuyResultDto class add

---
+++ src/main/java/de/sha/ticketpie/dto/BuyResultDto.java
@@ -0,0 +1,13 @@
+package de.sha.ticketpie.dto;
+
+public class BuyResultDto {
+
+	public Boolean judgement;
+	public String seatNo;
+
+	public BuyResultDto(Boolean judgement, String seatNo) {
+		this.judgement = judgement;
+		this.seatNo = seatNo;
+	}
+
+}
