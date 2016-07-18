f68e0cde47514c15dab49d80af73a66459c5e12f
shade <sha-dev@gmail.com>
2016-07-18 16:13:55 +0900

    [add] add repositoryEventHandler class
    TicketEventHandler
    TcketSoldHistoryEventHandler

---
+++ src/main/java/de/sha/ticketpie/repository/eventhandler/TicketEventHandler.java
@@ -0,0 +1,19 @@
+package de.sha.ticketpie.repository.eventhandler;
+
+import java.util.Date;
+
+import org.springframework.data.rest.core.annotation.HandleBeforeSave;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
+
+import de.sha.ticketpie.entity.Ticket;
+
+@Component
+@RepositoryEventHandler
+public class TicketEventHandler {
+
+	@HandleBeforeSave
+	public void beforeSave(Ticket ticket){
+		ticket.updateDateTime = new Date();
+	}
+}
